6097dcdbcb5628e24d30938f57291d7a24b71360
Rick <dirklo@gmail.com>
2021-05-07 19:54:49 +0200

    clean up json responses

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,4 +5,19 @@ respond_to :json
         @team = Team.find_by(id: params['teamId']) 
         render json: @team.projects
     end
+
+    def create
+        @project = Project.new(project_params)
+        if @project.save
+            render json: {message: 'Project successfully saved', status: 200, project: @project}
+        else
+            render json: {message: 'Something went wrong!', status: 500}
+        end
+        
+    end
+
+    private
+        def project_params
+            params.require(:project).permit(:title, :creator_id, :team_id)
+        end
 end
