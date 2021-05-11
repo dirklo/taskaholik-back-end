024830a084b32d5776ab9036dcee82b56f1e43e7
Rick <dirklo@gmail.com>
2021-05-11 07:37:54 +0200

    Heroku Deploy

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -266,7 +266,7 @@ Devise.setup do |config|
   config.navigational_formats = []
 
   config.jwt do |jwt|
-    jwt.secret = Rails.application.credentials.secret_key_base
+    jwt.secret = ENV['SECRET_KEY_BASE']
     jwt.request_formats = {
       landlord: [nil, :json]
     }
