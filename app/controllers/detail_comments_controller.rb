361d8bb71876d1c3fc3f4f897d8828d4f7b1ff34
Rick <dirklo@gmail.com>
2021-05-29 20:56:54 +0200

    fix comment system

--- app/controllers/detail_comments_controller.rb
+++ app/controllers/detail_comments_controller.rb
@@ -4,17 +4,21 @@ class DetailCommentsController < ApplicationController
 
     def index
         @detail = Detail.find_by(id: params['detail_id'])
-        render json: @detail.comments
+        render json: @detail.comments.to_json(include: :author)
     end
 
     def create
-        @comment = DetailComment.create(detail_comment_params)
-        render json: {comment: @comment, author: @comment.author.username}
+        @comment = DetailComment.new(detail_comment_params)
+        @comment.author = current_user
+        if @comment.save
+            render json: @comment.to_json(include: :author), status: 200
+        else
+            render json: {message: @comment.errors.full_messages[0]}, status: 500
+        end
     end
 
     def destroy
         @detail_comment = DetailComment.find_by(id: params[:id])
-        @user = User.find_by(id: detail_comment_params['user_id'])
         if @detail_comment.destroy
             render json: {message: "Comment successfully deleted"}, status: 200
         else
--- app/controllers/task_comments_controller.rb
+++ app/controllers/task_comments_controller.rb
@@ -4,22 +4,22 @@ class TaskCommentsController < ApplicationController
 
     def index
         @task = Task.find_by(id: params['taskId'])
-        render json: @task.comments
+        render json: @task.comments.to_json(include: :author)
     end
 
     def create
         @comment = TaskComment.new(task_comment_params)
+        @comment.author = current_user
         if @comment.save
-            render json: {comment: @comment, author: @comment.author.username}, status: 200
+            render json: @comment.to_json(include: :author), status: 200
         else
             render json: {message: @comment.errors.full_messages[0]}, status: 500
         end
     end
 
     def destroy
-        @user = User.find_by(id: task_comment_params[:user_id])
         @task_comment = TaskComment.find_by(id: params[:id])
-        if @user == current_user && @user == @task_comment.author
+        if current_user == @task_comment.author
             if @task_comment.destroy
                 render json: {message: "Comment successfully deleted."}, status: 200
             else
